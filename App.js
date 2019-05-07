3a400a2bc0316947778694027e506ed08221cd98
Candogan Tuzcu <candogan.tuzcu@gmail.com>
2019-05-07 22:27:31 +0300

    Add switch navigator screen

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import React, { Component } from 'react';
 import {
   createStackNavigator,
   createMaterialTopTabNavigator,
-  createAppContainer
+  createAppContainer,
+  createSwitchNavigator
 } from 'react-navigation';
 
 import TrackingHome from './src/screens/Tracking/TrackingHome';
@@ -20,6 +21,7 @@ import AddPlants from './src/screens/Tracking/AddPlants';
 import RemovePlants from './src/screens/Tracking/RemovePlants';
 import Community from './src/screens/Community/Community';
 import Plantpedia from './src/screens/Plantpedia/Plantpedia';
+import Entrance from './src/screens/Auth/Entrance';
 import PlantImage from './src/components/PlantImage/PlantImage';
 import TabBar from './src/components/UI/TabBar/TabBar';
 
@@ -59,15 +61,41 @@ const PlantpediaStack = createStackNavigator(
   { initialRouteName: 'Home' }
 );
 
-export default createAppContainer(
-  createMaterialTopTabNavigator(
-    {
-      Tracking: { screen: TrackingStack },
-      Community: { screen: CommunityStack },
-      Plantpedia: { screen: PlantpediaStack }
-    },
-    {
-      tabBarComponent: TabBar
+const AuthStack = createStackNavigator(
+  {
+    Home: Entrance
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerBackTitle: null,
+      headerTintColor: '#004734',
+      backgroundColor: '#F5F5F5',
+      // headerForceInset: { top: 'never', bottom: 'never' },
+      headerTransparent: true
     }
-  )
+  }
 );
+
+const AppTabNavigator = createMaterialTopTabNavigator(
+  {
+    Tracking: { screen: TrackingStack },
+    Community: { screen: CommunityStack },
+    Plantpedia: { screen: PlantpediaStack }
+  },
+  {
+    tabBarComponent: TabBar
+  }
+);
+
+const SwitchNavigator = createSwitchNavigator(
+  {
+    App: AppTabNavigator,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: 'Auth'
+  }
+);
+
+export default createAppContainer(SwitchNavigator);
