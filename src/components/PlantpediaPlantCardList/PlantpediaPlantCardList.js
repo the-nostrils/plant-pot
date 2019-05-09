39bb4c2f4aa6da3207e4da7ae592aea8ce09e963
vai91 <mehmetefeekiner@gmail.com>
2019-05-09 22:37:32 +0200

    Configure contidional image rendering

--- src/components/PlantpediaPlantCardList/PlantpediaPlantCardList.js
+++ src/components/PlantpediaPlantCardList/PlantpediaPlantCardList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {
-  Alert,
   FlatList,
   ScrollView,
   StyleSheet,
@@ -16,78 +15,161 @@ export default class PlantpediaPlantCardList extends Component {
 
   state = {
     plantList: [
-      { id: 1, name: 'Onion', size: 'small' },
-      { id: 2, name: 'Beet', size: 'medium' },
-      { id: 3, name: 'Strawberry', size: 'large' },
-      { id: 4, name: 'Onion', size: 'medium' },
-      { id: 5, name: 'Beet', size: 'large' },
-      { id: 6, name: 'Strawberry', size: 'small' }
+      {
+        id: 1,
+        name: 'Onion',
+        size: 'small',
+        levelOfExpertise: 1,
+        season: 'sow',
+        plantTime: [3, 4],
+        harvestTime: [7, 8, 9],
+        family: 'Liliaceae',
+        waterNeedLevel: 'medium',
+        commonProblems: [
+          'Leek Rust',
+          'Onion White Rot',
+          'Onion Downy Mildew'
+        ],
+        exposure: 'High',
+        climate: '16-24°C'
+      },
+      {
+        id: 2,
+        name: 'Spinach',
+        size: 'medium',
+        levelOfExpertise: 1,
+        season: 'sow',
+        plantTime: [3, 4, 5, 8, 9],
+        harvestTime: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        family: 'Amaranthaceae',
+        waterNeedLevel: 'high',
+        commonProblems: [
+          'Birds',
+          'Bolting',
+          'Spinach downy mildew'
+        ],
+        exposure: 'Winter cultivars need sunny position',
+        climate: '16-18°C'
+      },
+      {
+        id: 3,
+        name: 'Garlic',
+        size: 'small',
+        levelOfExpertise: 1,
+        season: 'sow',
+        plantTime: [1, 2, 10, 11, 12],
+        harvestTime: [6, 7, 8],
+        family: 'Herbaceous',
+        waterNeedLevel: 'medium',
+        commonProblems: [
+          'Onion white rot',
+          'Leek rust',
+          'Birds'
+        ],
+        exposure: 'High',
+        climate: '13-24°C'
+      },
+      {
+        id: 4,
+        name: 'Lettuce',
+        size: 'small',
+        levelOfExpertise: 1,
+        season: 'sow',
+        plantTime: [3, 4, 5, 6, 7, 8, 9],
+        harvestTime: [5, 6, 7, 8, 9, 10, 11, 12],
+        family: 'Asteraceae',
+        waterNeedLevel: 'medium',
+        commonProblems: [
+          'Slugs and snails',
+          'Grey mould',
+          'Bolting'
+        ],
+        exposure: 'High',
+        climate: '2°C (Min.)'
+      },
+      {
+        id: 5,
+        name: 'Brussel Sprouts',
+        size: 'medium',
+        levelOfExpertise: 2,
+        season: 'sow',
+        plantTime: [2, 3, 4, 5],
+        harvestTime: [1, 2, 3, 9, 10, 11, 12],
+        family: 'Crucifers',
+        waterNeedLevel: 'high',
+        commonProblems: [
+          'Birds',
+          'Club Root',
+          'Cabbage Root Fly'
+        ],
+        exposure: 'High',
+        climate: '16-18°C'
+      },
+      {
+        id: 6,
+        name: 'Parsley',
+        size: 'small',
+        levelOfExpertise: 1,
+        season: 'sow',
+        plantTime: [3, 4, 5, 6],
+        harvestTime: [6, 7, 8],
+        family: 'Apiaceae',
+        waterNeedLevel: 'high',
+        commonProblems: [
+          'Carrot Fly',
+          'Celery Leaf Miner',
+          'Carrot Motley Dwarf Virus'
+        ],
+        exposure: 'Winter cultivars need sunny position',
+        climate: '7°C (Min.)'
+      }
     ],
     searchQuery: ''
-  };
-
-  plantButtonPressedHandler = (buttonMode) => {
-    const { navigation } = this.props;
-
-    switch (buttonMode) {
-      case 0:
-        Alert.alert('Add Plants modal will open.');
-        // navigation.navigate('AddPlants');
-        break;
-      case 1:
-        // Alert.alert('Go to Remove Plants Screen!');
-        navigation.navigate('RemovePlants');
-        break;
-      default:
-        Alert.alert('TrackingHome');
-    }
-  };
-
-  removeButtonPressedHandler = () => {
-    Alert.alert('Remove button pressed!');
   };
 
   searchHandler = (val) => {
     this.setState({
       searchQuery: val
     });
-  }
-  // plantCardPressedhandler = () => this.  props.navigation.navigate('Details');
+  };
 
   render() {
-    // const { listButtonMode } = this.props;
-    const { plantList } = this.state;
-    const { searchQuery } = this.state;
-
+    const { plantList, searchQuery } = this.state;
     const plantCardWidth = 340;
 
     return (
-        <ScrollView>
-          <View style={styles.container}>
-            <View style={styles.searchBoxContainer}>
-              <SearchBox
-                searchQuery={searchQuery}
-                searchHandler={val => this.searchHandler(val)}
-              />
-            </View>
-            <View style={styles.cardListContainer}>
-              <FlatList
-                data={plantList}
-                keyExtractor={item => item.id.toString()}
-                renderItem={({ item }) => (
-                  <View style={styles.plantCardContainer}>
-                    <PlantpediaPlantCard
-                      name={item.name}
-                      width={plantCardWidth}
-                      size={item.size}
-                      {...this.props}
-                    />
-                  </View>
-                )}
-              />
-            </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.searchBoxContainer}>
+            <SearchBox
+              searchQuery={searchQuery}
+              searchHandler={val => this.searchHandler(val)
+              }
+            />
           </View>
-        </ScrollView>
+          <View style={styles.cardListContainer}>
+            <FlatList
+              data={plantList}
+              keyExtractor={item => item.id.toString()
+              }
+              renderItem={({ item }) => (
+                <View
+                  style={
+                    styles.plantCardContainer
+                  }
+                >
+                  <PlantpediaPlantCard
+                    name={item.name}
+                    width={plantCardWidth}
+                    size={item.size}
+                    {...this.props}
+                  />
+                </View>
+              )}
+            />
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
